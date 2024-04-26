6b7298b1a1fd83ebc2650aa0056f127719acd1fd
Jozef Cipa <cipa.jozef@gmail.com>
2024-04-26 17:49:26 +0200

    feat: add colors to the output

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/arsham/figurine/figurine"
+	cc "github.com/ivanpirog/coloredcobra"
 	"github.com/jozefcipa/novus/internal/fs"
 	"github.com/jozefcipa/novus/internal/logger"
 	"github.com/spf13/cobra"
@@ -13,29 +16,38 @@ var rootCmd = &cobra.Command{
 	Version: "-",
 	Use:     "novus",
 	Short:   "Local web development done effortlessly",
-	Long: ` _   _
-| \ | | _____   ___   _ ___
-|  \| |/ _ \ \ / / | | / __|
-| |\  | (_) \ V /| |_| \__ \
-|_| \_|\___/ \_/  \__,_|___/
-
-Novus is a tool that improves developer experience when working
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		fs.ResolveDirs()
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		figurine.Write(os.Stdout, "Novus", "ANSI Regular.flf")
+		fmt.Println(`Novus is a tool that improves developer experience when working
 on one or multiple web services by automatically providing
 SSL secured URLs that proxy traffic to your services.
 
-No more http://localhost:3000 calls.
+That means no more http://localhost:3000 calls.
 Instead, open the "novus.yml" configuration and add a nice custom domain name
 that will forward all the traffic to your upstream service.
+To start run "novus serve --create-config" to initialize Novus and create an example configuration.`)
 
-To start run "novus serve --create-config" to initialize Novus and create an example configuration.
-`,
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		fs.ResolveDirs()
+		cmd.Help()
 	},
 }
 
 func Execute(version string) {
+	// show app version
 	rootCmd.SetVersionTemplate(version)
+
+	// configure colors
+	cc.Init(&cc.Config{
+		RootCmd:  rootCmd,
+		Headings: cc.HiCyan + cc.Bold + cc.Underline,
+		Commands: cc.HiYellow + cc.Bold,
+		Example:  cc.Italic,
+		ExecName: cc.Bold,
+		Flags:    cc.Bold,
+	})
+
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
--- go.mod
+++ go.mod
@@ -3,23 +3,29 @@ module github.com/jozefcipa/novus
 go 1.21.0
 
 require (
+	github.com/arsham/figurine v1.3.0
 	github.com/fatih/color v1.16.0
 	github.com/go-playground/validator/v10 v10.18.0
+	github.com/ivanpirog/coloredcobra v1.0.1
 	github.com/spf13/cobra v1.8.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/arsham/rainbow v1.2.1 // indirect
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
-	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
