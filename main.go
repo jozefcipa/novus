f18882018fcc5a9cf047c97dc858e57a1e81e28c
Jozef Cipa <cipa.jozef@gmail.com>
2024-05-22 19:29:45 +0200

    chore: fix binary build time

--- main.go
+++ main.go
@@ -11,10 +11,13 @@ import (
 // These values are replaced when building the binary by GoReleaser
 var (
 	version = "DEVELOPMENT"
-	date    = time.Now().Format("2006-01-02")
+	date    = ""
 )
 
 func main() {
+	if date == "" {
+		date = time.Now().Format("2006-01-02")
+	}
 	versionString := fmt.Sprintf("Novus %s (%s) on %s/%s\n", version, date, runtime.GOOS, runtime.GOARCH)
 	cmd.Execute(versionString)
 }
